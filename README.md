3b114493949e3fbb2cc64416edb73932ae38f277
brisberg <brandon.risberg@gmail.com>
2018-05-13 00:44:46 -0700

    Added simple tests which run in Roblox Studio

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ro01
+
+
+Run unit tests in Roblox Studio with the following in the console:
+
+game:GetService("TestService"):Run()
---
+++ src/simple.spec.lua
@@ -0,0 +1,9 @@
+return function()
+	it("should sdfsfs pass", function()
+		expect(true).to.equal(true)
+	end)
+
+    it("should fail", function()
+		expect(true).to.equal(false)
+	end)
+end
